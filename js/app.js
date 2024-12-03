8979e16a30109950582f86ca0b5d9ceecdb5afde
Nowone <lamtom3301@gmail.com>
2024-12-03 16:12:01 +0100

    jeu memory fonctionnel

--- js/app.js
+++ js/app.js
@@ -5,8 +5,13 @@ const grid = document.createElement('section')
 grid.setAttribute('class', 'grid')
 // Insert le plateau de carte dans la zone de jeu definit
 game.appendChild(grid)
-//creer une carte
+
+
+
 let cards=[];
+let score = 0;
+let firstCard, secondCard;
+let lockBoard = false;
 
 // recuperer les données pour alimenter chaque carte
 fetch("./js/legume.json")
@@ -35,16 +40,20 @@ function generateCards(){
         
         // on lui ajoute la class card pour le css dédié
         card.classList.add('card');
+        card.setAttribute("data-name", item.name)
         // on récupère son nom donnée dans le json
         card.dataset.image = item.image;
         card.dataset.name = item.name;
         card.innerHTML=`
-        <div >
-            <img class="card" src="${item.image}" alt="${item.name}"/>
-        </div> `;
+        <div class="front">
+            <img class="front-image" src="${item.image}" alt=" carte ${item.name}"/>
+        </div>
+        <div class="back"></div>
+         `;
 
         // on l'insert dans la grille de jeu
         grid.appendChild(card)
+        card.addEventListener("click", flipCard);
     
 
 });
@@ -56,7 +65,7 @@ function shuffleCards(){
     let temporaryValue;
 
     while(currentIndex !==0){
-        randomIndex = Math.floor(Math.random()*currentIndex);
+        randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex --;
 
         temporaryValue = cards[currentIndex];
@@ -66,3 +75,75 @@ function shuffleCards(){
     }
 }
 
+
+function flipCard(){
+    if(lockBoard){
+        return;
+    }
+    if(this === firstCard){
+        return;
+    }
+    this.classList.add("flipped");
+
+    if(!firstCard){
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    score ++;
+    document.querySelector(".score").textContent = score;
+    lockBoard = true;
+
+    checkForMatch();
+}
+
+function checkForMatch(){
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    if(isMatch){
+        disableCard();
+    }else{
+        unflipCard();
+    } 
+
+    function disableCard(){
+
+        firstCard.removeEventListener("click",flipCard);
+        secondCard.removeEventListener("click", flipCard);
+
+        resetBoard();
+    }
+ 
+    function unflipCard(){
+        setTimeout(() => {
+            firstCard.classList.remove("flipped");
+            secondCard.classList.remove("flipped");
+            resetBoard();
+        }, 1000);
+    }    
+
+    
+}
+
+
+function resetBoard(){
+        firstCard = null;
+        secondCard = null;
+        lockBoard = false;
+    }
+
+
+function restart(){
+    resetBoard();
+    shuffleCards();
+    score = 0;
+    document.querySelector(".score").textContent = score;
+    grid.innerHTML ="";
+    generateCards();
+}
+
+document.addEventListener("keydown", (event) => {
+    if(event.code === 'Space'){
+        restart();
+    }
+})
